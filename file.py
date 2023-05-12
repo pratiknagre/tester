d3110a99c45a5c818590464e683c8fbc01e3205d
N1 <pathaknishant003@gmail.com>
2023-05-12 12:31:17 -0400

    add

---
+++ file.py
@@ -0,0 +1,7 @@
+Pandas is a Python library for data manipulation and analysis. It provides a fast and efficient way to handle structured data, such as spreadsheets and databases, in a variety of formats, including CSV, Excel, SQL databases, and JSON.
+
+Pandas allows you to read, write, and manipulate data in many ways, such as indexing, selecting, filtering, grouping, merging, and reshaping. It also provides tools for data visualization and time series analysis.
+
+The two primary data structures in Pandas are Series and DataFrame. A Series is a one-dimensional array-like object that can hold any data type, including numerical, string, and boolean values. A DataFrame is a two-dimensional table-like structure that consists of rows and columns, where each column can have a different data type.
+
+To use Pandas, you first need to import the library by running the command import pandas as pd. From there, you can start manipulating data using the various functions and methods provided by the library.
